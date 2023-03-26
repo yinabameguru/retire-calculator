6bcfb95fc0f733381851b71cf0adf6d28a810122
yinabameguru <764904418@qq.com>
2023-03-27 00:59:43 +0800

    init

---
+++ src/main/java/cn/com/meguru/retirecalculator/RetireCalculatorApplication.java
@@ -0,0 +1,13 @@
+package cn.com.meguru.retirecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetireCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetireCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/com/meguru/retirecalculator/RetireCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.meguru.retirecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetireCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
